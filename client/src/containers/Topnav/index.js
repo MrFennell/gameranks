604122848bdfaf3e3d8d9cfc240ebff9fb41f25f
Ryan <rfennell001@gmail.com>
2020-01-14 16:35:37 -0500

    Fix for bootstrap-react nav

--- client/src/containers/Topnav/index.js
+++ client/src/containers/Topnav/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { logout } from 'actions/session';
 import { Link } from 'react-router-dom';
-import Search from './Search'
+import { LinkContainer } from 'react-router-bootstrap'
 
+import Search from './Search'
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 
 const mapStateToProps = ({ session, games }) => ({
     session,
@@ -26,20 +27,19 @@ class Topnav extends React.Component{
         if(user){
             return(
                 <div className="container">
-                    <Navbar expand="lg">
+                    <Navbar expand="md">
                         <Navbar.Brand><Link Link to='/dashboard'>Gameranks</Link></Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="mr-auto">
                                 {/* <Nav.Link><Link to='/dashboard'>{user}</Link></Nav.Link> */}
                                 <NavDropdown title={user} id="basic-nav-dropdown">
-                                    <NavDropdown.Item><Link to='/dashboard'>Profile</Link></NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                    <LinkContainer to='/dashboard'><NavDropdown.Item>Dashboard</NavDropdown.Item></LinkContainer>
                                     <NavDropdown.Item onClick={this.props.logout}>Logout</NavDropdown.Item>
                                     <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                    {/* <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
                                 </NavDropdown>
-                                <Nav.Link><Link to='/games'>Games</Link></Nav.Link>
+                                <LinkContainer to='/games'><Nav.Link>Games</Nav.Link></LinkContainer>
                                 {/* <Button onClick={this.props.logout} variant="secondary">Logout</Button> */}
                                 
                             </Nav>
@@ -59,10 +59,9 @@ class Topnav extends React.Component{
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link><Link to='/login'>Login</Link> </Nav.Link>
-                            <Nav.Link><Link to='/signup'>Signup</Link></Nav.Link>
-                            <Nav.Link><Link to='/games'>Games</Link></Nav.Link>
-                            
+                            <LinkContainer to='/login'><Nav.Link>Login</Nav.Link></LinkContainer > 
+                            <LinkContainer to='/signup'><Nav.Link>Signup</Nav.Link></LinkContainer>
+                            <LinkContainer to='/games'><Nav.Link>Games</Nav.Link></LinkContainer>
                         </Nav>
                             <Form inline>
                                 <Search history={this.props.history} />
